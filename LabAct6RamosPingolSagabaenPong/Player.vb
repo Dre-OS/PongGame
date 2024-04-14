cbb71076b06192ad0f48bd23590ae4c1e24d6ea6
Trent <hlsagabaen@gmail.com>
2024-04-14 21:51:27 +0800

    added class player atrib and methods

--- LabAct6RamosPingolSagabaenPong/Player.vb
+++ LabAct6RamosPingolSagabaenPong/Player.vb
@@ -1,9 +1,56 @@
 ﻿Public Class Player
-    Dim score As Integer
-    Dim ip As String
-    Dim local As Boolean
+    Private Nname As String
+    Private Sscore As Integer
+    Private Pposition As Point
+    Private MmovementStep As Integer
 
-    Sub New()
+    Public Property Name As String
+        Get
+            Return Nname
+        End Get
+        Set(value As String)
+            Nname = value
+        End Set
+    End Property
 
+    Public Property Score As Integer
+        Get
+            Return Sscore
+        End Get
+        Set(value As Integer)
+            Sscore = value
+        End Set
+    End Property
+
+    Public Property Position As Point
+        Get
+            Return Pposition
+        End Get
+        Set(value As Point)
+            Pposition = value
+        End Set
+    End Property
+
+    Public Property MovementStep As Integer
+        Get
+            Return MmovementStep
+        End Get
+        Set(value As Integer)
+            MmovementStep = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal name As String, ByVal position As Point, ByVal movementStep As Integer)
+        Nname = name
+        Pposition = position
+        MmovementStep = movementStep
+    End Sub
+
+    Public Sub MoveUp()
+        Pposition.Y = Math.Max(0, Pposition.Y - MmovementStep)
+    End Sub
+
+    Public Sub MoveDown(ByVal formHeight As Integer, ByVal playerHeight As Integer)
+        Pposition.Y = Math.Min(formHeight - playerHeight, Pposition.Y + MmovementStep)
     End Sub
 End Class
